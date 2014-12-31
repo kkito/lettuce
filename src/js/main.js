fe2505d3dfb02b79ee35c4e662b0e2d96adf2278
kk.shen <kk.shen@saybot.com>
2014-12-31 21:38:55 +0800

    post link

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,20 @@
-React.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('example')
-);
+var PostLink = React.createClass({
+    render: function(){
+        return <li><a href={this.props.url}>{this.props.title}</a></li>;
+    }
+})
+
+var PagePostLinks = React.createClass({
+    render: function(){
+        var links = this.props.posts.map(function(post){
+            return <PostLink url={post.url} title={post.title} />
+        })
+        return <ul>{links}</ul>
+    }
+})
+
+// React.render(
+//     <h1>Hello, world!<PagePostLinks posts={[{url: "wfe" , title:"1222"} , {url: "232" , title: "ggg"}]} /></h1>,
+//     document.getElementById('example')
+// );
 
