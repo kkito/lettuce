9be67e943b203d4cda4b25c53761b1158c081fcd
kk.shen <kk.shen@saybot.com>
2015-01-01 12:57:32 +0800

    list index page use react

--- src/js/main.js
+++ src/js/main.js
@@ -13,6 +13,24 @@ var PagePostLinks = React.createClass({
     }
 })
 
+var IndexPosts = React.createClass({
+    maxSize: 20,
+    groupPosts: function(){
+        var result  = []
+        var posts = this.props.posts;
+        for(var i = 0 ; i < posts.length ; i += this.maxSize) {
+            result.push(posts.slice(i , i + this.maxSize))
+        }
+        return result;
+    },
+    render: function(){
+        var pages = this.groupPosts().map(function(posts){
+            return <PagePostLinks posts={posts} />
+        })
+        return <div>{pages}</div>
+    }
+})
+
 // React.render(
 //     <h1>Hello, world!<PagePostLinks posts={[{url: "wfe" , title:"1222"} , {url: "232" , title: "ggg"}]} /></h1>,
 //     document.getElementById('example')
